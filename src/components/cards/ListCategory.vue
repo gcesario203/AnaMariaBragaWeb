26e8a083e19974269ba261d5b2339eae6601e839
Gabriel Cesario <cesario203@outlook.com>
2020-09-15 10:27:40 -0300

    Finalizando estilização da listagem de categorias

--- src/components/cards/ListCategory.vue
+++ src/components/cards/ListCategory.vue
@@ -1,32 +1,76 @@
 <template>
-    <div class="list__category">
-        <li
-            v-for="cat in Categories"
-            :key="cat.id"
+   <div class="category__filtered">
+       <h1>Receitas encontradas</h1>
+       <div
+            class="category__list"
+            v-for="category in Categories"
+            :key="category.id"
+            @click="changeInputFilterCategoryValue(category.strCategory); filterByCategory()"
         >
-            <h3
-                @click="
-                    changeInputFilterCategoryValue(cat.strCategory);
-                    filterByCategory()
-                "
-            >
-                {{cat.strCategory}}
-            </h3>
-        </li>
-    </div>
+                <h1>{{category.strCategory}}</h1>
+                <v-img
+                    :src="category.strCategoryThumb"
+                    max-width="80vw"
+                    max-height="30vh"
+                    v-model="page"
+                ></v-img>
+                <h3>{{category.strCategoryDescription}}</h3>
+                
+       </div>
+   </div>
 </template>
 
 <script>
-import {mapState,mapActions} from 'vuex'
+import {mapActions, mapState} from 'vuex'
 export default {
     name:'ListCategory',
     computed:mapState(['Categories']),
-    methods:{
-        ...mapActions(['filterByCategory', 'changeInputFilterCategoryValue'])
-    }
+    methods:{ ...mapActions(['filterByCategory', 'changeInputFilterCategoryValue']) }
 }
 </script>
 
 <style>
+.category__filtered{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
+.category__filtered>h1{
+    width: 100%;
+    text-align: center;
+    font-family: 'Grandstander';
+    color: rgb(80, 80, 80);
+}
+
+.category__list{
+    margin: 2rem 0 1rem 0;
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: grey;
+    border-radius: 1rem;
+    transition: 800ms;
+}
+
+.category__list > h1{
+    width: 100%;
+    text-align: center;
+    font-family: 'Grandstander';
+    padding-bottom: 2rem;
+}
+
+.category__list > h3{
+    font-family: 'Lato';
+    width: 100%;
+    text-align: center;
+    padding-top: 2rem;
+}
+
+.category__list:hover{
+    background: rgb(235, 131, 35);
+    cursor: pointer;
+}
 </style>
