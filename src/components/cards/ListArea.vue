9ac9e505718fccc37c48469dde7954b142bebf0b
Gabriel Cesario <cesario203@outlook.com>
2020-09-15 11:14:08 -0300

    Finalizando projeto com a estilização da listagem de areas

--- src/components/cards/ListArea.vue
+++ src/components/cards/ListArea.vue
@@ -29,5 +29,32 @@ export default {
 </script>
 
 <style>
+.list__area{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+}
 
+.list__area > li{
+    list-style: none;
+    background: rgb(139, 139, 139);
+    width: 100%;
+    max-width: 20rem;
+    padding: 2rem;
+    margin: 1rem 0;
+    border-radius: 1rem;
+    transition: 800ms;
+}
+
+.list__area > li:hover{
+    background: rgb(235, 131, 35);
+    cursor: pointer;
+}
+
+.list__area > li > h3{
+    font-family: 'Lato';
+    text-align: center;
+}
 </style>
